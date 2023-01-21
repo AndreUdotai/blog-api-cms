4b709fbf7d50f0b8e89d9c42bf3f0956974ac221
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-01-22 00:40:35 +0100

    refactored codes

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,75 @@
-async function getData(url) {
+// *******************************************************************************
+// UI COMPONENT GENEREATOR FUNCTIONS
+// *******************************************************************************
+// Function to generate USERS LIST COMPONENT
+let generateUsersListComponent = (username) => {
+    return  `<li class="row align-items-center my-2">
+                <p class="col rounded">${username}</p>
+                <div class="col d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button type="button" class="btn btn-primary">Edit</button>
+                </div>
+            </li>
+            <hr>`;
+};
+
+// Function to generate DASHBOARD COUNTS COMPONENT
+let generateDashboardComponent = (count1, count2, count3) => {
+    return `<div class="row p-3 m-5 rounded text-left" id="dashboard-display">
+                <div class="col m-1 dashboard-items rounded p-5">
+                    <div class="h1" id="post-count">${count1}</div>
+                    <div class="number-description">Total Posts</div>
+                </div>
+                <div class="col m-1 dashboard-items rounded p-5">
+                    <div class="h1" id="user-count">${count2}</div>
+                    <div class="number-description">Total Users</div>
+                </div>
+                <div class="col m-1 dashboard-items rounded p-5">
+                    <div class="h1" id="comment-count">${count3}</div>
+                    <div class="number-description">Total Comments</div>
+                </div>
+            </div>`;
+};
+
+// Function to generate the POST LIST COMPONENT
+let generatePostListComponent = (title, id, publishStatus) => {
+    return `<li class='row align-items-center my-2'>
+                <div class='col'>${title}</div>
+                <div class='col d-grid gap-2 d-md-flex justify-content-md-end'>
+                    <button
+                        type='button'
+                        class='btn btn-primary'
+                        data-id='${id}'
+                        data-value='${publishStatus}'
+                        id='publishButton'
+                    >
+                        ${publishStatus}
+                    </button>
+                    <button type='button' class='btn btn-primary'>
+                        Edit
+                    </button>
+                </div>
+            </li>
+            <hr>`;
+};
+
+// Function to generate the LOADING ANIMATION COMPONENT
+let generateLoadingComponent = () => {
+    return `<div class="h-100 d-flex align-items-center justify-content-center">
+                <div class="spinner-grow" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>`;
+};
+
+// *******************************************************************************
+// FETCH FUNCTIONS
+// *******************************************************************************
+
+// General fetch function to call fetch() taking the url and the fetch method as arguments
+async function getData(url, method) {
     const response = await fetch(url, {
         mode: 'cors',
-        method: 'GET',
+        method: method,
         withCredentials: true,
         credentials: 'omit',
         headers: {
@@ -18,50 +86,37 @@ async function getData(url) {
 
 // The fetch function that will run when the page loads
 // This function fetches the json data for the dashboard counter display
-let dashboardDisplay = () => {
+let fetchCounts = () => {
     // select the display node
     let display = document.getElementById('display');
-    // Create a loader animation component
-    let loaderElement = `<div class="h-100 d-flex align-items-center justify-content-center">
-                      <div class="spinner-grow" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                      </div>
-                    </div>`;
-    // Set the display innerHTML to the loader animation
-    display.innerHTML = loaderElement;
 
-    getData('http://localhost:3000/api/dashboard').then((data) => {
-        let username = document.getElementById('username');
-        username.innerText = data.authData.user.username;
-        // Create the dashboard count component
-        let dashboardComponent = `<div class="row p-3 m-5 rounded text-left" id="dashboard-display">
-                                        <div class="col m-1 dashboard-items rounded p-5">
-                                            <div class="h1" id="post-count">${data.results.post_count}</div>
-                                            <div class="number-description">Total Posts</div>
-                                        </div>
-                                        <div class="col m-1 dashboard-items rounded p-5">
-                                            <div class="h1" id="user-count">${data.results.user_count}</div>
-                                            <div class="number-description">Total Users</div>
-                                        </div>
-                                        <div class="col m-1 dashboard-items rounded p-5">
-                                            <div class="h1" id="comment-count">${data.results.comment_count}</div>
-                                            <div class="number-description">Total Comments</div>
-                                        </div>
-                                    </div>`;
-        // Set the innerHTML of the display element to show the dashbaordComponent
-        display.innerHTML = dashboardComponent;
+    // Set the display innerHTML to the loading animation component
+    display.innerHTML = generateLoadingComponent();
+
+    getData('http://localhost:3000/api/dashboard', 'GET').then((data) => {
+        // Display the username of the logged in User
+        displayUsername(data.authData.user.username);
+        
+        // Embed the dashboardComponent inside display node
+        display.innerHTML = generateDashboardComponent(data.results.post_count, data.results.user_count, data.results.comment_count);
     });
 };
 // Invoke the dashboardDisplay function on page load
-dashboardDisplay();
+fetchCounts();
 
+// Display the username of the logged in user
+let displayUsername = (username) => {
+    let usernameDisplay = document.getElementById('username');
+    return usernameDisplay.innerText = username;
+}
+
+// the fetchPosts function to retrieve all the posts from the DB
 let fetchPosts = () => {
-    getData('http://localhost:3000/api/posts')
+    getData('http://localhost:3000/api/posts', 'GET')
         // .then((response) => response.json())
         .then((data) => {
-            // Select the username element and set it to the username from the authData
-            let username = document.getElementById('username');
-            username.innerText = data.authData.user.username;
+            // Display the username of the logged in User
+            displayUsername(data.authData.user.username);
 
             // Create a new Ul element that will contain the post list
             let postListContainer = document.createElement('ul');
@@ -80,32 +135,10 @@ let fetchPosts = () => {
                 let postCapsule = document.createElement('div');
 
                 // Set published status button display according to its value
-                let publishStatus = post.published
-                    ? 'Published'
-                    : 'Unpublished';
-
-                // Create the post list HTML component
-                let postListComponent = `<li class='row align-items-center my-2'>
-                                                    <div class='col'>${post.title}</div>
-                                                    <div class='col d-grid gap-2 d-md-flex justify-content-md-end'>
-                                                        <button
-                                                            type='button'
-                                                            class='btn btn-primary'
-                                                            data-id='${post._id}'
-                                                            data-value='${publishStatus}'
-                                                            id='publishButton'
-                                                        >
-                                                            ${publishStatus}
-                                                        </button>
-                                                        <button type='button' class='btn btn-primary'>
-                                                            Edit
-                                                        </button>
-                                                    </div>
-                                                </li>
-                                                <hr>`;
+                let publishStatus = post.published ? 'Published' : 'Unpublished';
 
                 // Embed the postListComponent inside post capsule element
-                postCapsule.innerHTML = postListComponent;
+                postCapsule.innerHTML = generatePostListComponent(post.title, post._id, publishStatus);
                 // Append post capsule on post list container for each iteration
                 postListContainer.appendChild(postCapsule);
             }
@@ -116,84 +149,13 @@ let fetchPosts = () => {
         });
 };
 
-// The post published status toggle function
-// Takes the post id and the status to be changed to as arguments
-let updatePublishedStatus = (id, status) => {
-    // A post request to update the post of 'id' with a new status
-    fetch(
-        'http://localhost:3000/api/post/' + id + '/published/update/' + status,
-        {
-            mode: 'cors',
-            method: 'POST',
-            withCredentials: true,
-            credentials: 'omit',
-            headers: {
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-                'Access-Control-Allow-Credentials': 'true',
-                'Access-Control-Allow-Origin': 'null',
-                Authorization:
-                    'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYzYjc3NmJmYTQ3ZTliMjZlOWYzM2U1OSIsInVzZXJuYW1lIjoibWFjbWlsbGVyIiwicGFzc3dvcmQiOiIkMmEkMTAkNnBMaU04UmxFaFdmdzUwZGFTS3l4dUtFQzNENnY3Y252Yi9tbmlZMENCd21aWldFb1p4NS4iLCJfX3YiOjB9LCJpYXQiOjE2NzMyOTYwNzZ9.mKcfDLi7i9ULynoVRCI3pJaM6nbr4mm74dIYdFks5pQ',
-                'Content-Type': 'application/json',
-            },
-        },
-    );
-};
-
-// *******************************************************************************
-// WHEN DASHBOARD BUTTON IS CLICKED
-// *******************************************************************************
-
-// Select the dashboard button
-let dashboardButton = document.getElementById('dashboard');
-// Add a click listener on the db button and call the dashbaordDisplay function
-dashboardButton.addEventListener('click', () => dashboardDisplay());
-
-// *******************************************************************************
-// WHEN POST LIST BUTTON IS CLICKED
-// *******************************************************************************
-
-// Select the post-list button element from the DOM
-let postListButton = document.getElementById('post-list');
-// Add a click listener to the button and call the fetchPosts function
-// to update the DOM with the posts list
-postListButton.addEventListener('click', () => fetchPosts());
-
-// *******************************************************************************
-// WHEN PUBLISHED STATUS BUTTON OF A POST IS CLICKED
-// *******************************************************************************
-
-// Select the display element from the DOM
-let display = document.getElementById('display');
-// Add a click listener to the display node
-display.addEventListener('click', (e) => {
-    // Set a condition to target a child node with the data-id attribute
-    // only published button has this attribute
-    if (e.target.hasAttribute('data-id')) {
-        // Set a condition to check innerText value, that is, the buttons name
-        if (e.target.dataset.value === 'Published') {
-            e.target.innerText = 'Unpublished';
-            // Call the updatePublished function with supplied arguments
-            updatePublishedStatus(e.target.dataset.id, 'false');
-        } else {
-            e.target.innerText = 'Published';
-            updatePublishedStatus(e.target.dataset.id, 'true');
-        }
-    }
-});
-
-// *******************************************************************************
-// WHEN USERS LIST BUTTON IS CLICKED
-// *******************************************************************************
-
-// Select the posts list element from the DOM
-let usersList = document.getElementById('users-list');
-usersList.addEventListener('click', () => {
+// The fetch Users function to retrieve all the users from the DB
+let fetchUsers = () => {
     getData('http://localhost:3000/api/users')
         // .then((response) => response.json())
         .then((data) => {
-            // Select the username element and set it to the username from the authData
-            let username = document.getElementById('username');
-            username.innerText = data.authData.user.username;
+            // Display the username of the logged in User
+            displayUsername(data.authData.user.username);
 
             // Create a new Ul element that will contain the users list
             let usersListContainer = document.createElement('ul');
@@ -211,17 +173,8 @@ usersList.addEventListener('click', () => {
                 // Create a div to hold the looped data
                 let userCapsule = document.createElement('div');
 
-                // Create the users list HTML component
-                let usersListComponent =    `<li class="row align-items-center my-2">
-                                                <p class="col rounded">${user.username}</p>
-                                                <div class="col d-grid gap-2 d-md-flex justify-content-md-end">
-                                                    <button type="button" class="btn btn-primary">Edit</button>
-                                                </div>
-                                            </li>
-                                            <hr>`;
-                
                 // Embed the usersListComponent inside user capsule element
-                userCapsule.innerHTML = usersListComponent;
+                userCapsule.innerHTML = generateUsersListComponent(user.username);
                 // Append user capsule on users list container for each iteration
                 usersListContainer.appendChild(userCapsule);
             }
@@ -230,4 +183,50 @@ usersList.addEventListener('click', () => {
             display.innerHTML = '';
             display.appendChild(usersListContainer);
         });
+}
+
+// The post published status toggle function
+// Takes the post id and the status to be changed to as arguments
+let updatePublishedStatus = (id, status) => {
+    let url = 'http://localhost:3000/api/post/' + id + '/published/update/' + status
+    // Call the getData function to initiate a post request to update the post of 'id' with a new 'status'
+    getData(url, 'POST');
+};
+
+// *******************************************************************************
+// CLICK EVENT LISTENERS
+// *******************************************************************************
+
+// WHEN DASHBOARD BUTTON IS CLICKED
+// Select and add a click event listener on the dashboard button to display counts
+let dashboardButton = document.getElementById('dashboard');
+dashboardButton.addEventListener('click', () => fetchCounts());
+
+// WHEN POST LIST BUTTON IS CLICKED
+// Select and add a click event listener on the post list button to display posts
+let postListButton = document.getElementById('post-list');
+postListButton.addEventListener('click', () => fetchPosts());
+
+// WHEN PUBLISHED STATUS BUTTON OF A POST IS CLICKED
+// Select and add a click event listener on the publish status button to update published status
+let display = document.getElementById('display');
+display.addEventListener('click', (e) => {
+    // Set a condition to target a child node with the data-id attribute (only published button has this attribute)
+    if (e.target.hasAttribute('data-id')) {
+        // Set a condition to check innerText value, that is, the buttons name
+        if (e.target.dataset.value === 'Published') {
+            // Toggle the name on the published status button
+            e.target.innerText = 'Unpublished';
+            // Call the updatePublished function with supplied arguments
+            updatePublishedStatus(e.target.dataset.id, 'false');
+        } else {
+            e.target.innerText = 'Published';
+            updatePublishedStatus(e.target.dataset.id, 'true');
+        }
+    }
 });
+
+// WHEN USERS LIST BUTTON IS CLICKED
+// Select and add a click event listener on the users list button to display users
+let usersList = document.getElementById('users-list');
+usersList.addEventListener('click', () => fetchUsers());
